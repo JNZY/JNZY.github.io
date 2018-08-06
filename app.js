8f472362bc6cd71056e9f62174adbe58d3f5f28f
John Roble <roble_john@outlook.com>
2018-08-06 20:17:00 +0800

    Improve functionality

--- app.js
+++ app.js
@@ -1,48 +1,54 @@
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer, dice, gamePlaying;
 
-scores = new Array(0,0);
-roundScore = 0;
-activePlayer = 0;
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+init();
 
 document.querySelector('.dice').style.display = 'none';
 
 document.querySelector('.btn-roll').addEventListener('click',function () {
-    dice = Math.floor(Math.random() * 6) + 1;
 
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + ".png";
+    if(gamePlaying) {
 
-    if(dice !== 1) {
-        roundScore += dice;
-        document.querySelector('#current-'+activePlayer).textContent = roundScore;
-    } else {
-        nextPlayer();
+        dice = Math.floor(Math.random() * 6) + 1;
+
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + ".png";
+    
+        if(dice !== 1) {
+            roundScore += dice;
+            document.querySelector('#current-'+activePlayer).textContent = roundScore;
+        } else {
+            nextPlayer();
+        }
     }
+
 
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
-    scores[activePlayer] += roundScore;
-    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
     
-    if(scores[activePlayer] >= 100) {
-
-        document.querySelector('#name-' +activePlayer).textContent = 'Winner!';
-        document.querySelector('.dice').style.display = 'none';
-        document.querySelector('.btn-roll').style.display = 'none';
-        document.querySelector('.btn-hold').style.display = 'none';
-
+    if(gamePlaying) {
+        scores[activePlayer] += roundScore;
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+        
+        if(scores[activePlayer] >= 10) {
+    
+            document.querySelector('#name-' +activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            // document.querySelector('.btn-roll').style.display = 'none';
+            // document.querySelector('.btn-hold').style.display = 'none';
+            document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
+            document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
+            gamePlaying = false;
+    
+        } else {
+            nextPlayer();
+        }
     }
-
-    nextPlayer();
-
+   
 });
+
+document.querySelector('.btn-new').addEventListener('click', init);
 
 function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
@@ -54,4 +60,24 @@ function nextPlayer() {
     document.querySelector('.player-1-panel').classList.toggle('active');
 
     document.querySelector('.dice').style.display = 'none';
+}
+
+function init() {
+
+    scores = new Array(0,0);
+    activePlayer = 0;
+    roundScore = 0;
+    gamePlaying = true;
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
 }
